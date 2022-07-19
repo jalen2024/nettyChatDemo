c6374e2a7d5c913b56791d6612e7b3c7f43ec1c5
jalen <jalen2024@163.com>
2022-07-19 14:21:50 +0800

    跑通长连接

--- app/src/main/java/com/freddy/chat/MainActivity.java
+++ app/src/main/java/com/freddy/chat/MainActivity.java
@@ -2,12 +2,89 @@ package com.freddy.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.freddy.chat.bean.SingleMessage;
+import com.freddy.chat.event.CEvent;
+import com.freddy.chat.event.CEventCenter;
+import com.freddy.chat.event.Events;
+import com.freddy.chat.event.I_CEventListener;
+import com.freddy.chat.im.IMSClientBootstrap;
+import com.freddy.chat.im.MessageProcessor;
+import com.freddy.chat.im.MessageType;
+import com.freddy.chat.utils.CThreadPoolExecutor;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements I_CEventListener {
+
+    private EditText mEditText;
+    private TextView mTextView;
+
+    String userId = "002";
+//    String userId = "116";
+    String token = "token_" + userId;
+    String hosts = "[{\"host\":\"192.168.0.103\", \"port\":8855}]";
+//    String hosts = "[{\"host\":\"192.168.0.116\", \"port\":8866}]";
+
+    private static final String[] EVENTS = {
+            Events.CHAT_SINGLE_MESSAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.et_content);
+        mTextView = findViewById(R.id.tv_msg);
+
+        IMSClientBootstrap.getInstance().init(userId, token, hosts, 1);
+
+        CEventCenter.registerEventListener(this, EVENTS);
+    }
+
+    public void sendMsg(View view) {
+        SingleMessage message = new SingleMessage();
+        message.setMsgId(UUID.randomUUID().toString());
+        message.setMsgType(MessageType.SINGLE_CHAT.getMsgType());
+        message.setMsgContentType(MessageType.MessageContentType.TEXT.getMsgContentType());
+        message.setFromId(userId);
+        message.setToId("001");
+//        message.setToId("102");
+        message.setTimestamp(System.currentTimeMillis());
+        message.setContent(mEditText.getText().toString());
+
+        MessageProcessor.getInstance().sendMsg(message);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CEventCenter.unregisterEventListener(this, EVENTS);
+    }
+
+    @Override
+    public void onCEvent(String topic, int msgCode, int resultCode, Object obj) {
+        switch (topic) {
+            case Events.CHAT_SINGLE_MESSAGE: {
+                final SingleMessage message = (SingleMessage) obj;
+                CThreadPoolExecutor.runOnMainThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        mTextView.setText(message.getContent());
+                    }
+                });
+                break;
+            }
+
+            default:
+                break;
+        }
     }
 }
